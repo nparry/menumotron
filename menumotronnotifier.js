7506ab193aa0edceb77ea4beb5c8a536fd6a5d67
Nathan Parry <nparry@gmail.com>
2017-02-26 19:25:53 -0500

    Lambda to send menu for the current day to hipchat

---
+++ menumotronnotifier.js
@@ -0,0 +1,42 @@
+var AWS = require('aws-sdk');
+var s3 = new AWS.S3();
+
+var Hipchatter = require('hipchatter');
+var hipchatter = new Hipchatter(process.env.HIPCHAT_AUTH_TOKEN, 'https://covermymeds.hipchat.com/v2/');
+
+var bucketName = 'menumotron.nparry.com';
+
+function sendHipchatMessage(message) {
+  hipchatter.notify('Menumotron', {
+    message: message,
+    color: 'green',
+    message_format: 'text',
+    token: process.env.HIPCHAT_ROOM_TOKEN,
+    notify: true
+  }, function(err) {
+    if (err == null) {
+      console.log('Sent hipchat notification');
+    }
+    else {
+      console.log('Failed to send hipchat notification');
+      console.log(err, err.stack);
+    }
+  });
+}
+
+exports.handler = function(event, context, callback) {
+  var today = new Date().toISOString().split('T')[0];
+  s3.getObject({
+    Bucket: bucketName,
+    Key: 'menus/' + today
+  }, function(err, data) {
+    if (err) {
+      console.log("Failed to fetch menu for " + today);
+      console.log(err, err.stack);
+      sendHipchatMessage("No menu found for " + today);
+    } else {
+      console.log("Fetched menu for " + today);
+      sendHipchatMessage(data.Body.utf8Slice());
+    }
+  });
+};
